1b03a8065c14ee27bd8ecb5c935d4a16b4e85cee
tech@onebee.in <tech@onebee.in>
2024-04-28 11:54:02 +0530

    basic

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import qrcode
 from io import BytesIO
-
 app = Flask(__name__)
-
 incoming_data = []
-
 def generate_qr_code(data):
     qr = qrcode.QRCode(
         version=1,
@@ -20,7 +17,6 @@ def generate_qr_code(data):
     img.save(img_io, format='PNG')
     img_io.seek(0)
     return img_io
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global incoming_data
@@ -28,7 +24,6 @@ def index():
         incoming_data.append(request.json.get('data'))
         return jsonify({'message': 'Data received successfully'}), 200
     return render_template('index.html', incoming_data=incoming_data)
-
 @app.route('/generate_qr_code', methods=['GET'])
 def get_qr_code():
     global incoming_data
@@ -37,6 +32,5 @@ def get_qr_code():
         img_io = generate_qr_code(data_to_encode)
         return img_io.getvalue(), 200, {'Content-Type': 'image/png'}
     return 'No data to generate QR code.', 400
-
 if __name__ == '__main__':
     app.run(debug=True)
